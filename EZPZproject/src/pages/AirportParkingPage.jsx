667b6afb1b353ac19e1339a355c693694b453fc4
cdh44 <carpediemcjs@naver.com>
2025-02-27 11:45:52 +0900

    0227 ui,버그 수정

--- EZPZproject/src/pages/AirportParkingPage.jsx
+++ EZPZproject/src/pages/AirportParkingPage.jsx
@@ -126,7 +126,7 @@ const AirportParkingPage = () => {
           <div className="description-section2">
           <h1>🚗 전국 공항 주차장 현황 🚗</h1>
                 <p className="checklist-alert">
-                    원활한 주차를 위한 각 공항별 주차장 현황도 입니다.
+                    원활한 주차를 위한 각 공항별 실시간 주차장 현황도 입니다.
                 </p>
             </div >
       
